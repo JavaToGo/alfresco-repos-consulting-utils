58ef0d3a9b3d93e2cfc1bbb3eb213b6651f0ec40
Richard Mcknight <richard.mcknight@alfresco.com>
2017-04-10 12:19:53 -0400

    tracking

--- src/main/java/org/alfresco/consulting/util/tracking/impl/TrackingComponentImpl.java
+++ src/main/java/org/alfresco/consulting/util/tracking/impl/TrackingComponentImpl.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.alfresco.consulting.util.tracking.TrackingComponent;
 import org.alfresco.repo.domain.node.NodeDAO;
 import org.alfresco.repo.domain.node.Transaction;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.attributes.AttributeService;
 import org.alfresco.service.cmr.repository.NodeRef.Status;
@@ -21,6 +23,13 @@ import org.alfresco.service.cmr.search.SearchService;
  */
 public class TrackingComponentImpl implements TrackingComponent {
 	private NodeDAO nodeDAO;
+	RetryingTransactionHelper txnHelper;
+	ServiceRegistry serviceRegistry;
+	
+	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+		this.serviceRegistry = serviceRegistry;
+		this.txnHelper = this.serviceRegistry.getRetryingTransactionHelper();
+	}
 
 	public void setNodeDAO(NodeDAO nodeDAO) {
 		this.nodeDAO = nodeDAO;
@@ -32,41 +41,77 @@ public class TrackingComponentImpl implements TrackingComponent {
 
 	@Override
 	public long firstTxnTimeStamp() {
-        nodeDAO.setCheckNodeConsistency();
-		List<Transaction> txnList = nodeDAO.getTxnsByCommitTimeAscending(0L, now(), 1, null, false);
-		return txnList.get(0).getCommitTimeMs();
+		return txnHelper.doInTransaction(new RetryingTransactionCallback<Long>(){
+
+			@Override
+			public Long execute() throws Throwable {
+		        nodeDAO.setCheckNodeConsistency();
+				List<Transaction> txnList = nodeDAO.getTxnsByCommitTimeAscending(0L, now(), 1, null, false);
+				return txnList.get(0).getCommitTimeMs();
+			}
+		});
 	}
 
 	@Override
-	public long getTimeStampOfLastTransactionBefore(long ts) {
-        nodeDAO.setCheckNodeConsistency();
-		long txnId = nodeDAO.getMaxTxnIdByCommitTime(ts-1);
-		return nodeDAO.getTxnById(txnId).getCommitTimeMs();
+	public long getTimeStampOfLastTransactionBefore(final long ts) {
+		return txnHelper.doInTransaction(new RetryingTransactionCallback<Long>(){
+
+			@Override
+			public Long execute() throws Throwable {
+		        nodeDAO.setCheckNodeConsistency();
+				long txnId = nodeDAO.getMaxTxnIdByCommitTime(ts-1);
+				return nodeDAO.getTxnById(txnId).getCommitTimeMs();
+			}
+		});
 	}
 
 	@Override
 	public long getTimeStampOfLastTransaction() {
-        nodeDAO.setCheckNodeConsistency();
-		return nodeDAO.getMaxTxnCommitTime();
+		return txnHelper.doInTransaction(new RetryingTransactionCallback<Long>(){
+
+			@Override
+			public Long execute() throws Throwable {
+		        nodeDAO.setCheckNodeConsistency();
+				return nodeDAO.getMaxTxnCommitTime();
+			}
+		});
 	}
 	
 
 	@Override
-	public List<Transaction> getTxnsByCommitTimeDescending(long fromTs, long toTs, int txnLimit) {
-        nodeDAO.setCheckNodeConsistency();
-		return nodeDAO.getTxnsByCommitTimeDescending(fromTs, toTs, txnLimit, null, false);
+	public List<Transaction> getTxnsByCommitTimeDescending(final long fromTs, final long toTs, final int txnLimit) {
+		return txnHelper.doInTransaction(new RetryingTransactionCallback<List<Transaction>>(){
+
+			@Override
+			public List<Transaction> execute() throws Throwable {
+		        nodeDAO.setCheckNodeConsistency();
+				return nodeDAO.getTxnsByCommitTimeDescending(fromTs, toTs, txnLimit, null, false);
+			}
+		});
 	}
 
 	@Override
-	public List<Transaction> getTxnsByCommitTimeAscending(long fromTs, long toTs, int txnLimit) {
-        nodeDAO.setCheckNodeConsistency();
-		return nodeDAO.getTxnsByCommitTimeAscending(fromTs, toTs, txnLimit, null, false);
+	public List<Transaction> getTxnsByCommitTimeAscending(final long fromTs, final long toTs, final int txnLimit) {
+		return txnHelper.doInTransaction(new RetryingTransactionCallback<List<Transaction>>(){
+
+			@Override
+			public List<Transaction> execute() throws Throwable {
+		        nodeDAO.setCheckNodeConsistency();
+				return nodeDAO.getTxnsByCommitTimeAscending(fromTs, toTs, txnLimit, null, false);
+			}
+		});
 	}
 
 	@Override
-	public List<Status> getTxnChanges(long txId) {
-        nodeDAO.setCheckNodeConsistency();
-		return nodeDAO.getTxnChanges(txId);
+	public List<Status> getTxnChanges(final long txId) {
+		return txnHelper.doInTransaction(new RetryingTransactionCallback<List<Status>>(){
+
+			@Override
+			public List<Status> execute() throws Throwable {
+		        nodeDAO.setCheckNodeConsistency();
+				return nodeDAO.getTxnChanges(txId);
+			}
+		});
 	}
 
 }
